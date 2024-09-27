6d98c31209f3c4d9cc2f7d7fe4eaef54dbe2c607
Wjink <arturorda77@gmail.com>
2024-09-27 12:43:24 +0300

    Basketball

---
+++ Masyvai/array.js
@@ -0,0 +1,19 @@
+const array1 = [];
+const array2 = new Array();
+const array3 = ['Text1', 'text2'];
+const array4 = new Array(4);
+const array5 = new Array(10).fill(0);
+
+const colors = ['red', 'green', 'blue'];
+
+const myPokemons = ['pikachu', 'Chavisard', 'bulbasaur', true, 8, [1, 2, 3]];
+
+
+const beasts = ['ant', 'bison', 'camel'];
+const animal1 = beasts[0], animal2 = beasts[1], animal3 = beasts[2];
+
+
+const arrayLength = beasts.length;
+const lastElement = beasts[beasts.length - 1];
+const randomElement = beasts[22];
+const bisonElemnt = beasts.indexOf('bison');
